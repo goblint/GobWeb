e3bea9f5f47fa19e1540e0a084fdbe19bde40cce
Ivana Zuzic <ivana.zuzic@tum.de>
2021-05-04 12:27:07 +0200

    Add copy citation

--- src/app/downloads/downloads.component.ts
+++ src/app/downloads/downloads.component.ts
@@ -9,7 +9,26 @@ export class DownloadsComponent implements OnInit {
 
   constructor() { }
 
+
   ngOnInit(): void {
+  }
+
+  copyMessage(elToCopy: string){
+    let myContainer = <HTMLInputElement>document.getElementById(elToCopy);
+    let val = myContainer.value;
+    console.log(myContainer.value);
+    val= "@inproceedings\{Goblint2016\,\}";
+    const selBox = document.createElement('textarea');
+    selBox.style.position = 'fixed';
+    selBox.style.left = '0';
+    selBox.style.top = '0';
+    selBox.style.opacity = '0';
+    selBox.value = val;
+    document.body.appendChild(selBox);
+    selBox.focus();
+    selBox.select();
+    document.execCommand('copy');
+    document.body.removeChild(selBox);
   }
 
 }
