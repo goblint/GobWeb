48242e37b5afb42ffae4a7e6306dd60813c3495a
Ivana Zuzic <ivana.zuzic@tum.de>
2021-05-06 01:52:42 +0200

    Improve copying of our main citation

--- src/app/downloads/downloads.component.ts
+++ src/app/downloads/downloads.component.ts
@@ -15,30 +15,19 @@ export class DownloadsComponent implements OnInit {
 
   copyMessage(elToCopy: string){
     let myContainer = <HTMLInputElement>document.getElementById(elToCopy);
-    let val = myContainer.value;
-    console.log(myContainer.value);
-    val= "\@inproceedings\{Goblint2016\,\n\
-      author    = \{Vesal Vojdani and Kalmer Apinis and Vootele R\{\\\~\{o\}\}tov and\n\
-                  Helmut Seidl and Varmo Vene and Ralf Vogler\}\,\n\
-      title     = \{Static race detection for device drivers: the Goblint approach\}\,\n\
-      booktitle = \{Proceedings of the 31st \{IEEE\/ACM\} International Conference on \n\
-                  Automated Software Engineering, \{ASE\} 2016\}\,\n\
-      pages     = \{391--402\}\,\n\
-      publisher = \{\{ACM\}\}\,\n\
-      year      = \{2016\}\,\n\
-      doi       = \{10.1145\/2970276.2970337\}\n\
-  \}";
-    const selBox = document.createElement('textarea');
-    selBox.style.position = 'fixed';
-    selBox.style.left = '0';
-    selBox.style.top = '0';
-    selBox.style.opacity = '0';
-    selBox.value = val;
-    document.body.appendChild(selBox);
-    selBox.focus();
-    selBox.select();
-    document.execCommand('copy');
-    document.body.removeChild(selBox);
+    var range = document.createRange();
+    range.selectNode(myContainer);
+    window.getSelection().addRange(range);
+    try
+    {
+        document.execCommand('copy');
+    } 
+    catch (err)
+    {
+        console.log('Unable to copy');
+    }
+
+    window.getSelection().removeRange(range);
   }
 
 }
