1a33f8a6a84d038f428225e2e59af5906628e0b8
Michael Schwarz <michael.schwarz93@gmail.com>
2023-01-24 13:42:10 +0100

    Undo changes to dissertations

--- src/app/dissertations.ts
+++ src/app/dissertations.ts
@@ -1,24 +1,24 @@
 import { Dissertation } from './dissertation';
 
 export const DISSERTATIONS: Dissertation[] = [
-  {
-    link: 'https://dspace.ut.ee/bitstream/handle/10062/15866/vesal_vojdani.pdf?sequence=1&isAllowed=y',
+  { 
+    link: 'http://www2.in.tum.de/bib/files/Vojdani10Thesis.pdf',
     title: 'Static Data Race Analysis of Heap-Manipulating C Programs',
     author: 'Vesal Vojdani',
     description: 'PhD thesis',
     university: 'University of Tartu',
-    date: 'December 2010'
+    date: 'December 2010.'
   },
-  {
-    link: 'https://mediatum.ub.tum.de/doc/1189191/1189191.pdf',
+  { 
+    link: 'http://www2.in.tum.de/bib/files/apinis14diss.pdf',
     title: 'Frameworks for analyzing multi-threaded C',
     author: 'Kalmer Apinis',
     description: 'PhD thesis',
     university: 'Technische Universität München',
     date: 'June 2014'
   },
-  {
-    link: 'https://www.dr.hut-verlag.de/978-3-8439-1956-2.html',
+  { 
+    link: 'http://www2.in.tum.de/bib/files/diss-schwarz.pdf',
     title: 'Static Analysis of Embedded Software with Priority Scheduling and Interrupts',
     author: 'Martin Schwarz',
     description: 'PhD thesis',
